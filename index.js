27d44b888659327ab04a83d18f84b100e760256a
Felix Hsu <xuchenfei@gmail.com>
2015-10-07 22:19:27 +0800

    refactor

--- index.js
+++ index.js
@@ -88,17 +88,49 @@ var startScrapeArticle = function(url) {
   var scrapeSite = new ScrapeSite(driver);
 
   scrapeSite.login0Day().then(function() {
-    return scrapeSite.scrapeArticle('http://www.0daydown.com/10/436427.html');
+    return scrapeSite.scrapeArticle(url);
   }).then(function(article) {
     return scrapeSite.saveArticle(article);
   }).then(function() {
     scrapeSite.logoff0Day();
+    driver.quit();
+    mongoose.disconnect();
   });
 }
 
 startScrape();
-/*Article.updateFilesExistence(["opSySaH1212.part1.rar", "opSySaH1212.part2.rar", "How_It_Works_Annual_-_Volume_6_2015-P2P.rar"], true).then(function(art) {
+//startScrapeArticle(args.article)
+/*Article.updateFilesExistence(["opSySaH1212.part1.rar", "opSySaH1212.part2.rar", "How_It_Works_Annual_-_Volume_6_2015-P2P.rar"], false).then(function(art) {
   console.log(art);
   mongoose.disconnect();
-});*/
+});
+
+Article.find({
+      $or:[{'baidupan.0': {$exists: false}},{'baidupan.f.0':{$exists: false}}],
+      $and:[{url:{$nin:["http://www.0daydown.com/06/303644.html", "http://www.0daydown.com/06/369155.html", "http://www.0daydown.com/06/375061.html", "http://www.0daydown.com/06/379035.html", "http://www.0daydown.com/06/379036.html", "http://www.0daydown.com/06/379118.html", "http://www.0daydown.com/06/379206.html", "http://www.0daydown.com/06/379241.html", "http://www.0daydown.com/06/379243.html", "http://www.0daydown.com/06/379253.html", "http://www.0daydown.com/06/379254.html", "http://www.0daydown.com/06/380061.html", "http://www.0daydown.com/06/380132.html", "http://www.0daydown.com/06/380133.html", "http://www.0daydown.com/07/18333.html" , "http://www.0daydown.com/07/306332.html", "http://www.0daydown.com/08/219737.html", "http://www.0daydown.com/08/328435.html", "http://www.0daydown.com/08/400233.html", "http://www.0daydown.com/08/403778.html", "http://www.0daydown.com/08/404492.html", "http://www.0daydown.com/08/406920.html", "http://www.0daydown.com/08/412137.html", "http://www.0daydown.com/08/414783.html", "http://www.0daydown.com/08/415932.html", "http://www.0daydown.com/08/80889.html" , "http://www.0daydown.com/09/419188.html", "http://www.0daydown.com/09/422243.html", "http://www.0daydown.com/09/49033.html" , "http://www.0daydown.com/09/90483.html"
+        ]}}, {category:{$ne:'Music/音乐'}}, {content: new RegExp("baidu", 'i')}]
+    }).then(function(arts){
+      var urls = _.pluck(arts, 'url');
+      console.log(urls);
+      var chromeCaps = webdriver.Capabilities.chrome();
+      chromeCaps.set('chromeOptions', {
+        'prefs': {
+          "profile.managed_default_content_settings.images": 2
+        }
+      });
+      var driver = new webdriver.Builder()
+        .withCapabilities(chromeCaps)
+        .build();
+      var baiduPan = new BaiduPan(driver);
+      var scrapeSite = new ScrapeSite(driver);
+
+      scrapeSite.login0Day().then(function() {
+        return scrapeSite.scrapeArticles(urls);
+      }).then(function() {
+        scrapeSite.logoff0Day();
+        driver.quit();
+        mongoose.disconnect();
+      });
+    })*/
+
 
