d2051e4e3241a3c44f12553513ba2b4743dd24cc
oats <oats@oats4.local>
2015-10-09 08:05:46 +0800

    start.sh changed

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for (( i=1500; i <= 3000; i+=200 ))
 do
-    echo "NODE_ENV=production node index.js --pages=200 --fromOffset=$i --browserNum=2"
-    NODE_ENV=production node index.js --pages=200 --fromOffset=$i --browserNum=2
+    echo "NODE_ENV=production node index.js --pages=100 --fromOffset=$i --browserNum=2 | tee r$i.log"
+    NODE_ENV=production node index.js --pages=100 --fromOffset=$i --browserNum=2 | tee r$i.log
 done
